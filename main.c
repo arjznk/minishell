dbe2016e5e567df31663994b1de3054df3c963e6
Rime Jebbari <rijebbar@f2r8s9.clusters.42paris.fr>
2026-05-18 16:41:39 +0200

    avant merge

---
+++ main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rijebbar <rijebbar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/18 15:25:14 by rijebbar          #+#    #+#             */
+/*   Updated: 2026/05/18 16:34:15 by rijebbar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int main(int ac, char **av)
+{
+    
+}
+
+// recup et stock l'env -> moi
+// areej -> tokenisation
+// une fois env stock -> relier avec les commandes pour l'exec
