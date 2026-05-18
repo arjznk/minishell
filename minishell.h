4a63d5627409cde3df210bc99f6a29a56edaabfa
Areej Zenk <azenk@f2r8s10.clusters.42paris.fr>
2026-05-18 15:18:24 +0200

    header

--- minishell.h
+++ minishell.h
@@ -6,12 +6,21 @@
 /*   By: azenk <azenk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 13:42:06 by azenk             #+#    #+#             */
-/*   Updated: 2026/05/18 13:42:41 by azenk            ###   ########.fr       */
+/*   Updated: 2026/05/18 13:43:55 by azenk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
 
 
 #endif
