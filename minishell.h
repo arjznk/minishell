75517ed09f6cc957e35b0e740d2b9f0beaac9a14
Rime Jebbari <rijebbar@f2r8s9.clusters.42paris.fr>
2026-05-18 15:17:58 +0200

    struct finis

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: rijebbar <rijebbar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 11:02:53 by rijebbar          #+#    #+#             */
-/*   Updated: 2026/05/18 11:06:23 by rijebbar         ###   ########.fr       */
+/*   Updated: 2026/05/18 15:17:20 by rijebbar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,19 @@
 #include <fcntl.h>
 #include <signal.h>
 
+typedef enum s_token_type 
+{
+    T_WORD,
+    T_PIPE,
+}   t_token_type;
 
+typedef struct s_token 
+{
+    char *str;
+    char *type;
+    int index;
+    struct s_token *next_token;
+} t_token;
 
 
 #endif
