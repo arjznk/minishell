3f3dc53ab243455a52acb0a130cb18240202aa1b
arjznk <arjznk@gmail.com>
2026-07-09 17:07:35 +0200

    merge

--- main.c
+++ main.c
@@ -45,11 +45,7 @@ int main(int ac, char **av, char **envp)
         while (tmp)
         {
             if (tmp->type == T_WORD)
-            {
-                printf("BEFORE=[%s]\n", tmp->str);
                 tmp->str = expand_and_remove_quotes(tmp->str, exec);
-                printf("AFTER=[%s]\n", tmp->str);
-            }
             tmp = tmp->next_token;
         }
         cmd = NULL;
